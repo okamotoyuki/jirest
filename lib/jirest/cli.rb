f1fee501c3088488f915ad57d6ef2788616200a4
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2019-09-17 23:59:50 +0900

    update command description

--- lib/jirest/cli.rb
+++ lib/jirest/cli.rb
@@ -7,31 +7,31 @@ module Jirest
 
   class Cli < Thor
 
-    desc "describe", "describe"
+    desc "describe", "Describe a Jira REST API."
     def describe
       command_generator = CommandExecutor.new
       command_generator.describe
     end
 
-    desc "update", "update"
+    desc "update", "Update all API information."
     def update
       api_def = Util::load_api_definition(DATA_DIR)
       ApiInfoUpdater.new(api_def).update
     end
 
-    desc "dryrun", "dryrun"
+    desc "dryrun", "Generate a curl command to call Jira REST API."
     def dryrun
       command_generator = CommandExecutor.new
       command_generator.dryrun
     end
 
-    desc "exec", "exec"
+    desc "exec", "Execute a curl command to call Jira REST API."
     def exec
       command_generator = CommandExecutor.new
       command_generator.exec
     end
 
-    desc "edit", "edit"
+    desc "edit", "Edit a template for Jira REST API."
     def edit
       command_generator = CommandExecutor.new
       command_generator.edit
